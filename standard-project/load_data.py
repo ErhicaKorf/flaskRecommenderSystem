a888f88b3c519b48992951c48610d1dc757cee7d
ErhicaKorf <59561547+ErhicaKorf@users.noreply.github.com>
2021-01-07 10:13:36 +0200

    load data, create recommender

---
+++ standard-project/load_data.py
@@ -0,0 +1,11 @@
+#%%
+import pandas as pd
+#load the data
+movie_data = pd.read_csv('C:/Users/erhicakorf/Documents/GitHub/recommender-system/standard-project/movies_metadata.csv', low_memory=False)
+movie_data = movie_data[0:20000]
+movie_data.head()
+
+# %%
+movie_data['overview'].head(10)
+
+# %%
